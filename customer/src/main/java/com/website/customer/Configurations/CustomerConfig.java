8ee1bceb0117aefbebfb2298936b73b36163d684
DanMelinte <Dmelinte2595@gmail.com>
2024-03-14 15:54:58 +0200

    customer changes

--- customer/src/main/java/com/website/customer/Configurations/CustomerConfig.java
+++ customer/src/main/java/com/website/customer/Configurations/CustomerConfig.java
@@ -1,6 +1,6 @@
 package com.website.customer.Configurations;
 
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerConfig {
 // example of
     @Bean
-//    @LoadBalanced
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
